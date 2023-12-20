5124352bc16312c46305e63408bbb4f3c012559f
ItanuRomero <itanromero2016@gmail.com>
2023-12-20 14:15:49 -0300

    feature(delete): create route parser and finish delete route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database/database.js';
+import { parseRoutePath } from './services/parse-route-path.js';
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: parseRoutePath('/users'),
         handler: (req, res) => {
             const users = database.select('users')
             return res
@@ -15,7 +16,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: parseRoutePath('/users'),
         handler: (req, res) => {
             if (req.body) {
                 const { name, email } = req.body
@@ -27,6 +28,22 @@ export const routes = [
                 return res.writeHead(201).end()
             }
             return res.writeHead(400).end('Request body was empty')
+        }
+    },
+    {
+        method: 'DELETE',
+        path: parseRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const id = req.params.id
+            if (id) {
+                try {
+                    database.delete('users', id)
+                    return res.writeHead(204).end()
+                } catch {
+                    return res.writeHead(404).end('User does not exist || already removed')
+                }
+            }
+            return res.writeHead(400).end('Id not specified')
         }
     }
 ]
